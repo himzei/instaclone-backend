e2af897797d5cc2e98cc84d48635c15dfcfaad57
himzei <himzei@gmail.com>
2021-03-24 21:03:13 +0900

    7.7 Subscriptions Setup part one

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectedResolver } from "./users/users.utils";
+import pubsub from "./pubsub";
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
@@ -20,6 +21,7 @@ const apollo = new ApolloServer({
 const app = express();
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
+apollo.installSubscriptionHandlers(app);
 app.use("/static", express.static("uploads"));
 app.listen({ port: PORT }, () => {
   console.log(`💄 Server is running on http://localhost:${PORT}`);
