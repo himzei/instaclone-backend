98d725680588ae9f307a6a810f03d9a490e28039
himzei <himzei@gmail.com>
2021-03-24 10:08:04 +0900

    6.6 seeHashtag

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -14,4 +14,19 @@ export default {
         },
       }),
   },
+  Hashtag: {
+    photos: ({ id }, { page }, { loggedInUser }) => {
+      return client.hashtag.findUnique({ where: { id } }).photos();
+    },
+    totalPhotos: ({ id }) =>
+      client.photo.count({
+        where: {
+          hashtags: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+  },
 };
