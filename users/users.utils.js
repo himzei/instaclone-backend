161b9a26987e9d8536566485c1a3f8edcb385e47
himzei <himzei@gmail.com>
2021-03-24 16:22:44 +0900

    6.18 protectedResolver Reflactor

--- users/users.utils.js
+++ users/users.utils.js
@@ -20,6 +20,11 @@ export const getUser = async (token) => {
 export function protectedResolver(ourResolver) {
   return function (root, args, context, info) {
     if (!context.loggedInUser) {
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      }
+    } else {
       return {
         ok: false,
         error: "Please log in to perform this action.",
