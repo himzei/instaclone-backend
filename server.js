06a8e819a1234f3f1ab60b11debc655fdff0721f
himzei <himzei@gmail.com>
2021-03-25 09:05:54 +0900

    7.8 Subscription Setup part Two

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 require("dotenv").config();
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
-import { getUser, protectedResolver } from "./users/users.utils";
-import pubsub from "./pubsub";
+import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-      protectedResolver,
-    };
+    if (req) {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    }
   },
 });
 
 const app = express();
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
-apollo.installSubscriptionHandlers(app);
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`💄 Server is running on http://localhost:${PORT}`);
 });
